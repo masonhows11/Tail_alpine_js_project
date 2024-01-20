d35f36537e7ba2efa2b43b0ba0e8ce532a4891df
mason.hows11 <mason.hows11@gmail.com>
2024-01-20 18:54:40 +0330

    first commit

---
+++ src/app/alpine_app.js
@@ -0,0 +1,40 @@
+// document.addEventListener('alpine:init', () => {
+//         Alpine.data('dropdown', () => ({
+//             open: false,
+ 
+//             toggle() {
+//                 this.open = ! this.open
+//             }
+//         }))
+//  })
+
+// document.addEventListener('alpine:init', () => {
+//     Alpine.data('myData', () => 
+//     ({
+//         //// define property
+//         message:'test apline from separate js file', 
+//         //// define array named : my_array
+//         my_array: ['hello','fucking','alpine js'],
+//         //// define function
+//         Myfunction()
+//         { 
+//             alert( this.message ) 
+//         }
+//     }))
+// })
+
+document.addEventListener('alpine:init', () => {
+    Alpine.data('globalData', () => ({
+       
+        users : [],
+
+        getUsers()
+        {
+                axios.get('https://jsonplaceholder.typicode.com/users').then((res)=>{
+                   this.users = res.data;
+                   console.log(this.users);
+                })
+        }
+
+    }))
+})
